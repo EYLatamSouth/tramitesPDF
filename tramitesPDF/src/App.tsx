673226586b687221a583c72cdeb6d168434f6f7e
santiagoescuderey <Santiago.Escuder@ar.ey.com>
2023-12-05 11:40:29 -0300

    first version

--- tramitesPDF/src/App.tsx
+++ tramitesPDF/src/App.tsx
@@ -1,33 +1,49 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Viewer, Worker, useIsomorphicLayoutEffect } from '@react-pdf-viewer/core';
+import '@react-pdf-viewer/core/lib/styles/index.css'
+/* import './App.css' */
+import { PDF } from './pdf';
+import usePDF from './hooks/usePDF';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const urlParams = new URLSearchParams(window.location.search);
+  const tramiteParam = urlParams.get("id");
+  const userParam = urlParams.get("usuario");
+
+  const { result } = usePDF(tramiteParam, userParam)
+
+  const base64toBlob = (data: string) => {
+    // Cut the prefix `data:application/pdf;base64` from the raw base 64
+    const base64WithoutPrefix = data.substring('data:application/pdf;base64,'.length);
+
+    const bytes = atob(base64WithoutPrefix);
+    let length = bytes.length;
+    let out = new Uint8Array(length);
+
+    while (length--) {
+      out[length] = bytes.charCodeAt(length);
+    }
+    console.log("RESULT ", result)
+    return new Blob([out], { type: 'application/pdf' });
+  };
+
+  const blob = base64toBlob(PDF);
+  const url = URL.createObjectURL(blob);
+  const fileName = "test.pdf";
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <div className='test'>
+{/*           <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+            <Viewer fileUrl={url} />
+          </Worker> */}
+          <embed src={`${url}#zoom=FitH`} width={1000} height={900}></embed>
+        </div>
+{/*         <div>
+          <a href={PDF} download={fileName}>Descargar PDF</a>
+        </div> */}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ tramitesPDF/src/hooks/usePDF.ts
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+//import { config as envConfig } from "../Entorno/Entorno";
+import { api } from "../services/config";
+// import data from "../apis/test-contratos-baja-3833.json";
+//import data from "../apis/test-response-userDetails.json";
+import axios from "axios";
+
+
+const usePDF = (tramite: string | null, usuario: string | null) => {
+  const [result, setResult] = useState({});
+ 
+  useEffect(() => {
+    //if (envConfig) {
+      /*                 Deploy                    */
+      axios.get(`https://osde-dev.prod.apimanagement.us10.hana.ondemand.com/v1/CPI/getGSC_PDF?tramite=${tramite}&usuario=${usuario}&system=C4C`, {
+        headers: {
+        "Content-Type": "application/json",
+        referer: "https://sap-integration-suite-y97uqswe.launchpad.cfapps.us10.hana.ondemand.com",
+        }
+    })
+
+        .then((response: any) => setResult(response))
+        .catch((error: any) => {
+          console.error(error);
+        });
+    //} else {
+      /*                 Local                    */
+      // setResult(data.results[0]);
+    //}
+  }, []);
+ 
+  return {
+    result
+  };
+};
+ 
+export default usePDF;
